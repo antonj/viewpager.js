25a0381a7655ea6d9a98f12e13a956790f806a5b
Anton Johansson <anton@department.se>
2015-03-09 23:34:17 +0100

    Console for ie

---
+++ src/console.js
@@ -0,0 +1,31 @@
+/*global module*/
+'use strict';
+
+function noop() {}
+
+module.exports = window.console || {
+  assert: noop,
+  clear: noop,
+  constructor: noop,
+  count: noop,
+  debug: noop,
+  dir: noop,
+  dirxml: noop,
+  error: noop,
+  group: noop,
+  groupCollapsed: noop,
+  groupEnd: noop,
+  info: noop,
+  log: noop,
+  markTimeline: noop,
+  profile: noop,
+  profileEnd: noop,
+  table: noop,
+  time: noop,
+  timeEnd: noop,
+  timeStamp: noop,
+  timeline: noop,
+  timelineEnd: noop,
+  trace: noop,
+  warn: noop
+};
